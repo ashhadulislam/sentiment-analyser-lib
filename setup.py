8a93801a5c9ff3a1c5f20f6c99958ab59bd03af5
ashhadulislam@gmail.com <ashhadulislam@gmail.com>
2019-05-08 16:51:22 +0530

    readme update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='sentimentanalyser',
-      version='2.1',
+      version='2.2',
       description='Generic python library to perform sentiment analysis on any dataset, using different models',
       long_description='A generic package to help developers perform analysis on their datasets, powered by SVM and '
                        'Naive-Bayes models and to be followed by many more.',
